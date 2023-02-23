74320b8922223a759a8399008e05508140a803d2
Basil <sharpshot101@hotmail.co.uk>
2023-02-23 13:57:14 +0000

    Char matching and storing

--- madLibs.js
+++ madLibs.js
@@ -1,26 +1,25 @@
 const imp_types = require("./types.js"); //access all verbs
 
+//story data. will need a seperate file for these later and then import them
 let str_storyTitle = "The Magic Tree";
 let str_story = "Damages the black nail and kill the stupid misty jam";
 
+//arrays
 let ar_story = str_story.split(/([_\W])/);
 let ar_alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let ar_type = 0; //changes the index of which reference array to use (for verbs, adjectives etc)
 
+//objects -> need to add new ones in here and in the set arrays function
 let obj_matchedTypes = {"matchVer":[], "matchAdj": []}; //object that stores all matched word types from a function. 
 let obj_types = {"verbRefs":imp_types.ar_REF_verbs, "adjRefs": imp_types.ar_REF_adjectives};
 
-let typesKeys = Object.keys(obj_types);
-let matchKeys = Object.keys(obj_matchedTypes);
-
-//this returns the first character of a string and its alphabet index as an object
+//Function: returns the first character of a provided string and its alphabet index as an object
 const getFirstChar = (theWord) => {
     let index = 0;
     let first = theWord[0]
 
     for(let i = 0; i < ar_alphabet.length ; i++)
     {
-        // console.log("---------->getfirstchar: loop-"+i+", character-"+ar_alphabet[i])
         if(first.toLowerCase() == ar_alphabet[i])
         {
             index = i;
@@ -29,44 +28,33 @@ const getFirstChar = (theWord) => {
     }
 
     let objRes = {"firstChar": first, "index": index}
-    // console.log("------------->objRes set as:")
-    // console.table(objRes)
-    // console.log("------------->\n")
     return objRes
 }
 
-//this function updates the arrays used to compare and store words, depending on the choice made when calling the findverbs function 
+//Function: updates the arrays used to compare and store matched words, depending on the choice made when calling the findverbs function 
 const setArrays = (type, wordReference) =>
 {
-    // console.log("\tsetArrays INTERNALS -> type: "+type+", wordReference: "+wordReference)
+    let targetAr = [];
+    let matchAr = [];
+    
     switch(type)
     {
         case 0:
-            // console.log("\tCase 0 selected - VERBS")
-            // console.log("\tgetFirstChar(wordReference).index = "+getFirstChar(wordReference).index)
-            targetArray = obj_types.verbRefs[getFirstChar(wordReference).index];
-            // console.log("\tCase 0, targetArray: "+targetArray)
-            chosenMatchAR = obj_matchedTypes.matchVer;
-            // console.log("\tCase 0, chosenMatchAR: "+chosenMatchAR)
+            targetAr = obj_types.verbRefs[getFirstChar(wordReference).index];
+            matchAr = obj_matchedTypes.matchVer;
             break;
         case 1:
-            // console.log("\tCase 1 selected - ADJECTIVES")
-            // console.log("\tgetFirstChar(wordReference).index = "+getFirstChar(wordReference).index);
-            targetArray = obj_types.adjRefs[getFirstChar(wordReference).index];
-            // console.log("\tCase 1, targetArray: "+targetArray)
-            chosenMatchAR = obj_matchedTypes.matchAdj;         
-            // console.log("\tCase 1, chosenMatchAR: "+chosenMatchAR)
+            targetAr = obj_types.adjRefs[getFirstChar(wordReference).index];
+            matchAr = obj_matchedTypes.matchAdj;         
             break
     }
-//     console.log("After calling setArrays:")
-// console.log(targetArray)
-// console.log(chosenMatchAR)
+    result = {"targetAr": targetAr, "matchAr":matchAr};
+    return result
 }
 
 const findVerbs = (inputArray, typeChoice) => {
 
 try{
-console.log("On entry to findVerbs, typeChoice was "+typeChoice)
 //set the typeChoice arrays
 let targetArray = [];
 let chosenMatchAR = [];
@@ -77,41 +65,33 @@ let chosenMatchAR = [];
         {
             //get the first word...
             let wordRef = inputArray[i];
-            setArrays(typeChoice, wordRef)
-console.log("On loop: ["+i+"], current word is " +wordRef)
-
-            //then select the subarray of verbs to search through (so match it alphabetically)...
-// console.log("\ttargetArray is: "+targetArray)
-
-            //now for every entry in the subarray...
+            //then choose the right arrays to compare against and put results in to
+            targetArray = setArrays(typeChoice, wordRef).targetAr
+            chosenMatchAR = setArrays(typeChoice, wordRef).matchAr
+            //now for every entry in the subarray chosen above...
             middleLoop:
             for(let o = 0; o < (targetArray.length-1); o++)
             {
+                //determine the nth character of the word provided above
                 let curVerb = targetArray[o] //character of word (from outer loop "o")
-                console.log("--->On middle loop: ["+o+"], curVerb is " +curVerb)
-                
-                //check each character matches the same index as the provided word
+                //then check each character matches the same index as the provided word
                 innerLoop:
                 for(let p = 0; p < (curVerb.length); p++)
                 {
-                    console.log("Loops i/o/p: ["+i+"/"+o+"/"+p+"], Word/Verb: ["+curVerb+"/"+wordRef+"], WordCha/RefCha: ["+wordRef[p]+"/"+curVerb[p]+"]")
-                    
+                    //if punctuation or a number is found on the current character, skip this loop
                     if(/[^a-zA-Z0-9\s]/.test(curVerb[p]))
                     {
-                        console.log(wordRef+" - Char was not alphabetic")
-                        continue //if punctuation or a number is found, skip this loop
+                        continue 
                     }
 
-                    //first, check index p is not greate than the length of the word or verb
+                    //check index is not greater than the length of the word or verb (so loop doesnt overflow and crash)
                     if(p >= curVerb.length || p >= wordRef.length)
                     {
-                        // console.log("---> INDEX OVERFLOW. BREAKING")
                         break
                     }
                     //then, check if characters match. if not, break this loop
                     if(curVerb[p].toLowerCase() != wordRef[p].toLowerCase())
                     {
-                        // console.log("---> CHARS DID NOT MATCH. BREAKING")
                         break
                     }
                     
@@ -119,24 +99,19 @@ console.log("On loop: ["+i+"], current word is " +wordRef)
                     else if((p+1) == curVerb.length && (p+1) >= (wordRef.length-2))
                     {
                         result = {"originalWord":wordRef, "matchedWord":curVerb, "iIndex":i, "typeChoice":typeChoice}
-                        console.log("A result was generated! ")
-                        console.log(result)
-                        console.log("-------------")
                         chosenMatchAR.push(result)
-console.log("------\n\tResult generated as: ")
-console.log(result)                       
-console.log("-----") 
                         break middleLoop
                     }
 
+                    //if the char matched AND the index didn't overflow, continue to the next loop
                     else if(curVerb[p].toLowerCase() === wordRef[p].toLowerCase() && (p+1) != curVerb.length)
                     {
                         continue
                     }
                     
+                    //if none of the above conditions are met, go to the next word
                     else
                     {
-                        // console.log("--->SoMe OtHeR CoNdItIoN")
                         break
                     }
                 }
@@ -154,6 +129,7 @@ console.log("-----")
             console.log("!=====--------------------------=====!\n")
         }
 }
+
 console.table(ar_story)
 findVerbs(ar_story, 0);
 findVerbs(ar_story, 1);
