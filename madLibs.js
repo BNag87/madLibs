0138c86154283d260620413a6d6631a51d9f28b1
Basil <sharpshot101@hotmail.co.uk>
2023-02-28 15:31:58 +0000

    Working verb, adj, noun capturing

--- madLibs.js
+++ madLibs.js
@@ -1,17 +1,17 @@
 const imp_types = require("./types.js"); //access all verbs
 
 //story data. will need a seperate file for these later and then import them
-let str_storyTitle = "The Magic Tree";
-let str_story = "Damages the black nail and kill the stupid misty jam";
+let str_storyTitle = "The Giant Arsehole";
+let str_story = "Wait, my beautiful excercise ball! Come and admire this charming hat!";
 
 //arrays
 let ar_story = str_story.split(/([_\W])/);
 let ar_alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let ar_type = 0; //changes the index of which reference array to use (for verbs, adjectives etc)
+let ar_CapAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 //objects -> need to add new ones in here and in the set arrays function
-let obj_matchedTypes = {"matchVer":[], "matchAdj": []}; //object that stores all matched word types from a function. 
-let obj_types = {"verbRefs":imp_types.ar_REF_verbs, "adjRefs": imp_types.ar_REF_adjectives};
+let obj_matchedTypes = {"matchVer":[], "matchAdj": [], "matchNou": []}; //object that stores all matched word types from a function. 
+let obj_types = {"verbRefs":imp_types.ar_REF_verbs, "adjRefs": imp_types.ar_REF_adjectives, "nouRefs":imp_types.ar_REF_nouns};
 
 //Function: returns the first character of a provided string and its alphabet index as an object
 const getFirstChar = (theWord) => {
@@ -20,11 +20,12 @@ const getFirstChar = (theWord) => {
 
     for(let i = 0; i < ar_alphabet.length ; i++)
     {
-        if(first.toLowerCase() == ar_alphabet[i])
+        if(first == ar_alphabet[i] || first == ar_CapAlphabet[i])
         {
             index = i;
             break
         }
+        else index = -1;
     }
 
     let objRes = {"firstChar": first, "index": index}
@@ -36,7 +37,7 @@ const setArrays = (type, wordReference) =>
 {
     let targetAr = [];
     let matchAr = [];
-    
+
     switch(type)
     {
         case 0:
@@ -47,6 +48,10 @@ const setArrays = (type, wordReference) =>
             targetAr = obj_types.adjRefs[getFirstChar(wordReference).index];
             matchAr = obj_matchedTypes.matchAdj;         
             break
+        case 2:
+            targetAr = obj_types.nouRefs[getFirstChar(wordReference).index];
+            matchAr = obj_matchedTypes.matchNou;         
+            break;
     }
     result = {"targetAr": targetAr, "matchAr":matchAr};
     return result
@@ -65,29 +70,40 @@ let chosenMatchAR = [];
         {
             //get the first word...
             let wordRef = inputArray[i];
+
+            //check that first character is actually an alphabetic character
             //then choose the right arrays to compare against and put results in to
-            targetArray = setArrays(typeChoice, wordRef).targetAr
-            chosenMatchAR = setArrays(typeChoice, wordRef).matchAr
+            if((/\P{L}/gu.test(wordRef) == true) || wordRef == "")
+                {
+                    continue
+                }
+                else
+                {
+                    targetArray = setArrays(typeChoice, wordRef).targetAr
+                    chosenMatchAR = setArrays(typeChoice, wordRef).matchAr
+                }
+
             //now for every entry in the subarray chosen above...
             middleLoop:
             for(let o = 0; o < (targetArray.length-1); o++)
             {
                 //determine the nth character of the word provided above
                 let curVerb = targetArray[o] //character of word (from outer loop "o")
+
                 //then check each character matches the same index as the provided word
                 innerLoop:
                 for(let p = 0; p < (curVerb.length); p++)
                 {
                     //if punctuation or a number is found on the current character, skip this loop
-                    if(/[^a-zA-Z0-9\s]/.test(curVerb[p]))
+                    if(/\P{L}/gu.test(wordRef[p] || wordRef[p] == ""))
                     {
-                        continue 
+                        break middleLoop
                     }
 
                     //check index is not greater than the length of the word or verb (so loop doesnt overflow and crash)
                     if(p >= curVerb.length || p >= wordRef.length)
                     {
-                        break
+                        break middleLoop
                     }
                     //then, check if characters match. if not, break this loop
                     if(curVerb[p].toLowerCase() != wordRef[p].toLowerCase())
@@ -96,7 +112,7 @@ let chosenMatchAR = [];
                     }
                     
                     //then, check if end has already been reached OR characters do not match
-                    else if((p+1) == curVerb.length && (p+1) >= (wordRef.length-2))
+                    if((p+1) == curVerb.length && (p+1) >= (wordRef.length-2))
                     {
                         result = {"originalWord":wordRef, "matchedWord":curVerb, "iIndex":i, "typeChoice":typeChoice}
                         chosenMatchAR.push(result)
@@ -104,16 +120,11 @@ let chosenMatchAR = [];
                     }
 
                     //if the char matched AND the index didn't overflow, continue to the next loop
-                    else if(curVerb[p].toLowerCase() === wordRef[p].toLowerCase() && (p+1) != curVerb.length)
+                    if(curVerb[p].toLowerCase() === wordRef[p].toLowerCase() && (p+1) != curVerb.length)
                     {
                         continue
                     }
                     
-                    //if none of the above conditions are met, go to the next word
-                    else
-                    {
-                        break
-                    }
                 }
             }
         }
@@ -130,8 +141,11 @@ let chosenMatchAR = [];
         }
 }
 
-console.table(ar_story)
 findVerbs(ar_story, 0);
 findVerbs(ar_story, 1);
+findVerbs(ar_story, 2);
+
+console.log("====================\n'"+str_storyTitle+"'\n====================")
 console.table(obj_matchedTypes.matchVer)
-console.table(obj_matchedTypes.matchAdj)  
+console.table(obj_matchedTypes.matchAdj)
+console.table(obj_matchedTypes.matchNou)    
